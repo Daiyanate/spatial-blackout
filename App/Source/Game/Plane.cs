9880a999995005751e03646e95989a2a63ec5b38
Daiyanate <daiyanate@gmail.com>
2025-05-20 18:49:31 +0200

    Project created

---
+++ App/Source/Game/Plane.cs
@@ -0,0 +1,15 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace TcGame
+{
+  public class Plane : StaticActor
+  {
+    public Plane()
+    {
+      Sprite = new Sprite(new Texture("Data/Textures/Player/Plane.png"));
+      Position = (Vector2f) Engine.Get.Window.Size / 2;
+    }
+   
+  }
+}
