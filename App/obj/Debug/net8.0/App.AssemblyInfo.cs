fcff06af67b4863f1c450fda6055a2b96b57af6b
NANEDCA <dhernandezr@edu.tecnocampus.cat>
2025-05-21 19:18:07 +0200

    tonto

--- App/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8a499be2bd96bfde95dc1f45cf3b8ba3f7aa84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5a9d648551075618d6671c1552b87adfb4887b")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
