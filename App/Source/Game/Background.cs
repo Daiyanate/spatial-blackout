16760bcbe830a0a9e9f630f0eed8f82ecea947ad
NANEDCA <dhernandezr@edu.tecnocampus.cat>
2025-05-21 21:10:44 +0200

    Blackout funcionant!
    implementació i millora de black out

--- App/Source/Game/Background.cs
+++ App/Source/Game/Background.cs
@@ -1,5 +1,6 @@
 ﻿using SFML.Graphics;
 using SFML.System;
+using System.Collections.Generic;
 
 namespace TcGame
 {
@@ -13,6 +14,53 @@ namespace TcGame
             Center();
 
             Position = new Vector2f(1920/2, 1080/2);
+      
+        
         }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt); 
+            BlackOut();
+        }
+        private void BlackOut()
+        {
+            List<Hud> Huds = Engine.Get.Scene.GetAll<Hud>();
+
+
+            foreach (Hud hud in Huds)
+            {
+                if (hud.time < 22.5 && hud.BckGrndTimer <= 5)
+                {
+                    Layer = ELayer.Front;
+
+                }
+                else
+                {
+                    Layer = ELayer.Background;
+                }
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
 }
--- App/Source/Game/Hud.cs
+++ App/Source/Game/Hud.cs
@@ -9,10 +9,10 @@ namespace TcGame;
 
 public class Hud : Actor
 {
-    Text t, t2, t3;
+    Text t, t2, t3,t4;
     Font f;
 
-    public float time = 20;
+    public float time = 27;
     private int time2;
     public float BckGrndTimer;
     public int Points;
@@ -27,7 +27,8 @@ public class Hud : Actor
     {
         Font f = new Font("Data/Fonts/LuckiestGuy.ttf");
         t = new Text("holaaa", f);
-        t2 = new Text("Points", f);
+        t2 = new Text("Points", f); 
+
         t2.Position = new Vector2f(Engine.Get.Window.Size.X / 2 + 170, 5);
         t.Position = new Vector2f(Engine.Get.Window.Size.X / 2 - 70, 5);
         t2.Scale *= 1.5f;
@@ -35,7 +36,10 @@ public class Hud : Actor
 
         t3 = new Text("Find the alien who is different from the rest!", f);
         t3.Position = new Vector2f(Engine.Get.Window.Size.X / 4 + 170, 150);
-
+        t4 = new Text("Blackout!!!!",f);
+        t4.Scale *= 2f;
+        t4.Position = new Vector2f(Engine.Get.Window.Size.X/2-60, Engine.Get.Window.Size.Y / 2) ;
+        
         Layer = ELayer.Hud;
         Center();
     }
@@ -52,6 +56,11 @@ public class Hud : Actor
         t2.Draw(target, states);
         t3.Draw(target, states);
 
+        if (time<22.5&&BckGrndTimer<=5)
+        {
+            t4.Draw(target, states);
+        }
+
         if (State == GameStates.GameOver)
         {
 
@@ -62,7 +71,8 @@ public class Hud : Actor
 
             }
 
-        }
+        } 
+        
 
 
     }
